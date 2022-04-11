b5a096fbc53e1d9570e10b5404c75027438cc9e1
isha <ishadixit642@gmail.com>
2022-04-12 03:27:30 +0530

    add mongodb mongoose

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // jshint esversion:6
 
-let items = [];
-let workItems = [];
+// let items = [];
+// let workItems = [];
+
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
+
+let day = date.getDate();
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -15,7 +22,70 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+
+const itemsSchema = {
+  name: {
+    type: String,
+    required: [true, "Error! Enter data correctly"]
+  }
+};
+
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your todo list!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];
+
+
+const listSchema = {
+  name : String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List",listSchema);
+
+
+
 app.get("/", function(req, res) {
+
+
+  Item.find({}, function(err, results) {
+
+    if (results.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("success");
+        }
+      });
+      res.redirect("/");
+    } else {
+
+      if (err) {
+        console.log(err);
+      } else {
+        //console.log(results);
+        res.render("list", {
+          listTitle: day,
+          newListItems: results
+        });
+      }
+    }
+  });
 
   let day = date.getDate();
   // var currentDay = today.getDay();
@@ -32,10 +102,6 @@ app.get("/", function(req, res) {
   //}
   // if(currentDay<0 || currentDay >6)
   // console.log("Error! Current Day is"+ currentDay);
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
-  });
   //res.send();
   //res.send("Hello");
 });
@@ -44,41 +110,106 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
   //   console.log(req.body);
   // console.log("hello");
-  let item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
+  let itemName = req.body.newItem;
+  let listName = req.body.listName;
+  // console.log(listName);
+  // console.log(itemName);
+
+if(listName === day){
+const item = new Item({
+  name : itemName
+});
+item.save();
+
+res.redirect("/");
+}
+else{
+  const item = new Item({
+    name : itemName
+  });
+  console.log(item);
+  List.findOneAndUpdate({name: listName},{$push: {"items": {name: itemName}}},function(err, foundList){
+    if(!err){
+      res.redirect("/"+listName);
+    }
+  });
+}
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
     // console.log(workItems);
     //res.redirect("/work");
-    let day = date.getDay();
-    res.render("list", {
-      listTitle: "Work List",
-      newListItems: workItems,
-      dayOfWeek: day
-    });
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  //   let day = date.getDay();
+  //   res.render("list", {
+  //     listTitle: "Work List",
+  //     newListItems: workItems,
+  //     dayOfWeek: day
+  //   });
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
   //console.log(req.body.newItem);
 });
 
 
-app.post("/work", function(req, res) {
-  // console.log(req.body);
-  // console.log("world");
-  let day = date.getDay();
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems,
-    dayOfWeek: day
+app.post("/delete",function(req,res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  console.log(listName);
+  console.log(day);
+
+if(listName === day){
+
+  Item.findByIdAndRemove(checkedItemId,function(err){
+    if(!err){
+      //console.log("success");
+
+      res.redirect("/");
+    }
   });
+}
+else{
+  console.log("hehe");
+  List.findOneAndUpdate({name: listName},{$pull: {items: {_id: checkedItemId}}},function(err, foundList){
+    if(!err){
+      res.redirect("/"+listName);
+    }
+  });
+}
 });
 
-// app.post("/work",function(req,res){
-//   let item = req.body.newItem;
-//   workItems.push(item);
-//   res.redirect("/work");
-// });
+
+app.get("/:customListName", function(req, res) {
+   const customListName = _.capitalize(req.params.customListName);
+
+List.findOne({name: customListName},function(err,results){
+  if(!err){
+    if(results){
+      res.render("list", {
+        listTitle: results.name,
+        newListItems: results.items
+      });
+    }
+    else {
+      const list = new List({
+        name : customListName,
+        items : defaultItems
+      });
+      list.save();
+      res.redirect("/"+customListName);
+    }
+  }
+});
+  // console.log("world");
+  // let day = date.getDay();
+  // res.render("list", {
+  //   listTitle: "Work List",
+  //   newListItems: workItems,
+  //   dayOfWeek: day
+  // });
+});
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running at 3000 speed!");
